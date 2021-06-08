36d6fa2e28c173a4e1213143671969372ffe7c00
Bismarck Renan <biusbiro@gmail.com>
2021-06-07 22:15:03 -0300

    first commit, initializing front dashboard for cmd process

---
+++ DashboardProcess/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace DashboardProcess.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
