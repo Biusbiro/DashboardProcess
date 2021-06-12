b7f20f0e7a89ea7b8908a10ee519397db6413022
Bismarck Renan <biusbiro@gmail.com>
2021-06-12 12:49:01 -0300

    addded ping method

---
+++ DashboardProcess/Models/ResultPing.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Threading.Tasks;
+
+namespace DashboardProcess.Models
+{
+    public class ResultPing
+    {
+        public int Id { get; set; }
+        public IPStatus Status { get; set; }
+        public int Ms { get; set; }
+
+    }
+}
---
+++ DashboardProcess/Services/ProcessService.cs
@@ -0,0 +1,32 @@
+﻿using DashboardProcess.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DashboardProcess.Services
+{
+    public class ProcessService
+    {
+        public ResultPing PingAddres(int round, String addres)
+        {
+            var result = new ResultPing();
+            Ping pingSender = new Ping();
+            PingOptions options = new PingOptions();
+            options.DontFragment = true;
+
+            byte[] buffer = Encoding.ASCII.GetBytes("");
+            int timeout = 120;
+            PingReply reply = pingSender.Send(addres, timeout, buffer, options);
+
+            result.Id = round;
+            result.Status = reply.Status;
+            result.Ms = reply.Options.Ttl;
+
+            return result;
+        }
+    }
+}
